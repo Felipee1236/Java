8cb2975cb1bca3949af4f6abfdd4bc4a012ce2de
Felipee1236 <161944185+Felipee1236@users.noreply.github.com>
2025-02-23 10:05:15 -0300

    Add files via upload

---
+++ part02-Part02_01.Squared/src/main/java/Squared.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class Squared {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        int result = num * num;
+        System.out.println(result);
+
+    }
+}
